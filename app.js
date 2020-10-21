785bbf91771afa7d54c745be0e56b96093f2d920
Elena Goreva <elena13.09.90@yandex.ru>
2020-10-21 17:52:36 +0300

    fix: App

--- app.js
+++ app.js
@@ -51,16 +51,13 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-  }).unknown(true),
+  }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^(https?:\/\/)?([a-zA-z0-9%$=?/.-]+)\.([a-zA-z0-9%$=?/.-]+)?(#)?$/),
   }).unknown(true),
 }), createUser);
 
