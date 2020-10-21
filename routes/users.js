f3d9670593b696a1b42f01e3bcacf3b81410ea1d
Elena Goreva <elena13.09.90@yandex.ru>
2020-10-21 18:12:48 +0300

    fix: routes

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/', getUsers);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().hex(),
+    userId: Joi.string().length(24).hex(),
   }),
 }), getUserById);
 
