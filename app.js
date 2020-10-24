bdaa32d625d252b264a761b0e2dfa23d16039052
Elena Goreva <elena13.09.90@yandex.ru>
2020-10-25 00:54:25 +0300

    fix: исправить замечания ревью

--- app.js
+++ app.js
@@ -49,14 +49,14 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().min(6),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().min(6),
   }).unknown(true),
 }), createUser);
 
